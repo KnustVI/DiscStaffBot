b8f0cf9d2addbe551d2261d029c75b012f03160f
KnustVI <vickwolfys2@gmail.com>
2026-03-27 12:38:22 -0300

    test

--- dashboard.js
+++ dashboard.js
@@ -116,7 +116,6 @@ function loadDashboard(client) {
     app.get('/manage/:guildID', checkAuth, async (req, res) => {
         try {
             const guildID = req.params.guildID;
-            console.log(client);
             const guild = client.guilds.cache.get(guildID);
             if (!guild) return res.redirect('/');
 
